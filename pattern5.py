651b1e68f96bb848d1c29a8d0eb3791e5f98c68b
Nishi Sharma <84862478+Nishi-Sharma24@users.noreply.github.com>
2022-03-24 20:26:13 +0530

    Add files via upload

---
+++ pattern5.py
@@ -0,0 +1,10 @@
+#pattern6
+for x in range(1,6):
+    for y in range(1,x+1):
+        print('*',end=' ')
+    print('\n')   
+for p in range(5,0,-1):        
+    for z in range(0,p-1):
+        print('*',end=' ')
+    print('\n')
+input() 
